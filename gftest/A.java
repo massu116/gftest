41b5316047ef0dd213b7f5a86093eb756308a4f3
masssu116 <kakeru1999116@yahoo.co.jp>
2019-07-11 10:06:58 +0900

    Aがキャラ移動を追加

--- gftest/A.java
+++ gftest/A.java
@@ -24,5 +24,9 @@ public class A extends Actor
         setRotation(0);
         move(1);
         }
+        if( Greenfoot.isKeyDown( "left" ) ){
+        setRotation(0);
+        move(-1);
+        }
     }    
 }
